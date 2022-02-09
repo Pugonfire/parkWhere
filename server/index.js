9a327c40e4506ff39cff3dad30b780d23a00b1d4
Max Z <maxsis100@gmail.com>
2022-02-09 16:36:04 +0800

    Created frontend and added static folder

--- server/index.js
+++ server/index.js
@@ -9,8 +9,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require("./routes/api/posts");
+const { sendFile } = require("express/lib/response");
 
 app.use("/api/posts", posts);
+
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + "/public"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => sendFile(__dirname + "/public/index.html"));
+}
 
 const port = process.env.PORT || 5000;
 
