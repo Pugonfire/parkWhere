1280dc1873306a80d7baeda86d6dc1b8697ea173
Ng Tze Sheng <ngtzesheng123@gmail.com>
2022-03-03 08:25:30 +0800

    added new route to googlemaps view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import NotFound from '@/views/NotFound.vue';
+import GoogleMap from '@/views/GoogleMap.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/googlemaps',
+    name: 'Google Maps',
+    component: GoogleMap,
   },
   {
     path: '/:catchAll(.*)',
