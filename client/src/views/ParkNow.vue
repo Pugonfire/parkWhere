06c543bd1944aadbbbaf93dbc0fdc4973081c8bd
Ng Tze Sheng <ngtzesheng123@gmail.com>
2022-03-04 21:45:58 +0800

    updated parknow

--- client/src/views/ParkNow.vue
+++ client/src/views/ParkNow.vue
@@ -4,12 +4,16 @@
     <div v-for="index in 2" :key="index">
       {{ pins[index] }}
     </div>
-    <h4>Click to update the map to your locality. (it may take some time to load)</h4>
+
+    <h4>Click to update the map to your locality. (Please wait for location to be retrieved)</h4>
     <button @click="getLocation">Get my Location</button>
+
     <h4>Drag the map around, click to console log carparks within the current map view.</h4>
     <button @click="logCurrentCarparks">Console Log Current Carparks</button>
+
     <h4>Drag the map around, click to console log carparks with their distances. (requires API key)</h4>
     <button @click="orderCarparks">Order carparks</button>
+
     <h4>Map</h4>
     <div id="map" style="width: 100%; height: 600px"></div>
   </div>
@@ -18,6 +22,8 @@
 <script>
 import { Loader } from '@googlemaps/js-api-loader';
 import CarparkPinService from '../CarparkPinService';
+import LocationServiceManager from '../managers/LocationServiceManager';
+import ParkNowManager from '../managers/ParkNowManager';
 
 export default {
   name: 'ParkNow',
@@ -42,6 +48,8 @@ export default {
       pins: [],
       candidates: [],
       myMarker: null,
+      locSvcMng: null,
+      location: null,
     };
   },
   async mounted() {
@@ -49,11 +57,13 @@ export default {
     await this.loadMapAPI();
     this.initMap();
     this.initMarkers();
+    this.locSvcMng = new LocationServiceManager();
+
     // Testing Code for Distance
     new this.google.maps.Marker({ position: this.center, title: 'Origin', map: this.map });
   },
   methods: {
-    orderCarparks() {
+    async orderCarparks() {
       this.candidates = [];
       this.pins.forEach((pin) => {
         if (this.map.getBounds().contains(pin.coords)) {
@@ -61,41 +71,8 @@ export default {
         }
       });
       this.candidates = [...new Map(this.candidates.map((item) => [item['ppCode'], item])).values()];
-      console.log(this.candidates);
 
-      let dest = [];
-      this.candidates.forEach((cp) => {
-        dest.push(cp.coords);
-      });
-      console.log(dest);
-
-      let service = new this.google.maps.DistanceMatrixService();
-      service.getDistanceMatrix(
-        {
-          origins: [this.center],
-          destinations: dest,
-          travelMode: 'DRIVING',
-        },
-        this.distanceCallback
-      );
-    },
-    distanceCallback(response, status) {
-      if (status == 'OK') {
-        var origins = response.originAddresses;
-        var destinations = response.destinationAddresses;
-
-        for (var i = 0; i < origins.length; i++) {
-          var results = response.rows[i].elements;
-          for (var j = 0; j < results.length; j++) {
-            var element = results[j];
-            var distance = element.distance.text;
-            var duration = element.duration.text;
-            var from = origins[i];
-            var to = destinations[j];
-            console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
-          }
-        }
-      }
+      await ParkNowManager.findCarpark(this.google, this.center, this.candidates);
     },
     async logCurrentCarparks() {
       this.candidates = [];
@@ -131,30 +108,31 @@ export default {
       });
     },
     getLocation() {
-      console.log("Getting User's Location");
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.center = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          console.log('Location Info Retrieved');
-          if (this.myMarker != null) {
-            this.myMarker.setPosition(this.center);
-          } else {
-            this.myMarker = new this.google.maps.Marker({
-              position: this.center,
-              title: 'You',
-              map: this.map,
-            });
-          }
-          this.map.setCenter(this.center);
-          this.map.setZoom(17);
-        },
-        (error) => {
-          console.log(error.message);
-        }
-      );
+      console.log(this.locSvcMng.requestLocationInfo());
+      // console.log("Getting User's Location");
+      // navigator.geolocation.getCurrentPosition(
+      //   (position) => {
+      //     this.center = {
+      //       lat: position.coords.latitude,
+      //       lng: position.coords.longitude,
+      //     };
+      //     console.log('Location Info Retrieved');
+      //     if (this.myMarker != null) {
+      //       this.myMarker.setPosition(this.center);
+      //     } else {
+      //       this.myMarker = new this.google.maps.Marker({
+      //         position: this.center,
+      //         title: 'You',
+      //         map: this.map,
+      //       });
+      //     }
+      //     this.map.setCenter(this.center);
+      //     this.map.setZoom(17);
+      //   },
+      //   (error) => {
+      //     console.log(error.message);
+      //   }
+      // );
     },
   },
 };
