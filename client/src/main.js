e6d97a919122b3031581a39872d7f8e87e311f28
Max Z <maxsis100@gmail.com>
2022-03-07 00:20:31 +0800

    Implemented login with google

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-//import GoogleSignInButton from 'vue-google-signin-button-directive';
+import gAuthPlugin from 'vue3-google-oauth2';
+
+let gAuth = {
+  clientId: '637708357057-0j7h0dup0bj7h51cndme96lrne4fqmqb.apps.googleusercontent.com',
+  scope: 'email',
+  prompt: 'consent',
+};
 
 const app = createApp(App);
 app.use(router);
+app.use(gAuthPlugin, gAuth);
 //app.use(GoogleSignInButton);
 app.mount('#app');
