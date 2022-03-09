2c6f19d4a7fcebf0bd077b43508f95e1ea6cb06d
Malthus Oh <malthusoh@gmail.com>
2022-03-09 10:40:52 +0800

    fusing JJ and Malthus search code

---
+++ client/src/components/CarparkCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p class="ppName">{{ carpark.ppName }}</p>
+    <p class="body">
+      <b>Carpark Code:</b> {{ carpark.ppCode }}
+      <br />
+      <b>Vehicle Type:</b> {{ carpark.vehCat }}
+      <br />
+      <b>Weekday Rates:</b> {{ carpark.weekdayRate }} per {{ carpark.weekdayMin }} <br /><b>Saturday Rates:</b>
+      {{ carpark.satdayRate }} per {{ carpark.satdayMin }} <br /><b>Sunday/Public Holiday Rates:</b>
+      {{ carpark.sunPHRate }} per {{ carpark.sunPHMin }} <br /><b>Effective start to end of parking rate:</b>
+      {{ carpark.startTime }} to
+      {{ carpark.endTime }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    carpark: {},
+  },
+};
+</script>
+
+<style></style>
---
+++ client/src/views/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Search</h1>
+    <div class="Searchbar">
+      <input type="text" name="userSearch" v-model="userSearch" placeholder="Enter name of carpark" />
+      <button @click="search">Search</button>
+    </div>
+    <hr />
+    <div class="carpark-container">
+      <div class="carpark" v-for="carpark in carparks.data" v-bind:item="carpark" v-bind:key="carpark._id">
+        <CarparkCard :carpark="carpark" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CarparkCard from '../components/CarparkCard.vue';
+export default {
+  data() {
+    return {
+      userSearch: '',
+      carparks: [],
+    };
+  },
+  components: {
+    CarparkCard,
+  },
+};
+</script>
+
+<style></style>
