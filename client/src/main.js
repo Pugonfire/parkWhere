ecd26293447aa2c7a1833ac1998bf4ad6dc99545
Max Z <maxsis100@gmail.com>
2022-03-17 17:06:44 +0800

    Standardise css file applying to all .vue files

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import gAuthPlugin from 'vue3-google-oauth2';
+import './assets/main.css';
 
 let gAuth = {
   clientId: '637708357057-0j7h0dup0bj7h51cndme96lrne4fqmqb.apps.googleusercontent.com',
