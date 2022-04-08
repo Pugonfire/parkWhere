8a6de0522cb214e38dfec124cb22bba51182329b
Ng Tze Sheng <ngtzesheng123@gmail.com>
2022-04-08 11:05:56 +0800

    https

--- server/index.js
+++ server/index.js
@@ -1,13 +1,3 @@
-/** Backend server entry point.
- * @module app
- * @requires express
- * @requires bodyParser
- * @requires cors
- * @requires database
- * @requires carpark_manager
- * @requires search_manager
- * @requires users_manager
- */
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -15,41 +5,48 @@ const database = require('./database_api');
 
 const app = express();
 
-/** Middleware. */
+const https = require('https');
+const fs = require('fs');
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-/**
- * Initialise singleton database instance.
- */
+// Initialise MongoDB singleton connection
 database.connectDB();
 
+// routes
+const posts = require('./routes/api/posts');
 const carparks = require('./routes/api/carpark_manager');
 const search = require('./routes/api/search_manager');
 const users = require('./routes/api/users_manager');
 
 const { sendFile } = require('express/lib/response');
 
+app.use('/api/posts', posts);
 app.use('/api/carpark_manager', carparks);
 app.use('/api/search_manager', search);
 app.use('/api/users_manager', users);
 
-/**
- * Handle Production.
- */
+// Handle production
 if (process.env.NODE_ENV === 'production') {
-  /**
-   * Static folder.
-   */
-  app.use(express.static(__dirname + '/public/'));
+  // Static folder
+  app.use(express.static(__dirname + '/public'));
 
-  /**
-   * Handle SPA.
-   */
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  // Handle SPA
+  app.get(/.*/, (req, res) => sendFile(__dirname + '/public/index.html'));
 }
 
-// const { port } = require('../config');
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert'),
+    },
+    app
+  )
+  .listen(port, () => console.log('HTTPS Server started on port ${port}'));
